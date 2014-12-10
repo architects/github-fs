604b58381e403a5a53fa4865263dbede42abad80
Jonathan Soeder <jonathan.soeder@gmail.com>
2014-12-10 15:39:53 -0600

    implementing a few more methods

--- lib/github-fs/file.rb
+++ lib/github-fs/file.rb
@@ -8,22 +8,59 @@ module GHFS
       end
     end
 
-    attr_reader :path, :mode, :options
+    attr_reader :path, :mode, :options, :repository, :branch
 
     def initialize(path, mode="w", options={})
-      @path = path
-      @mode = mode || "w"
-      @options = options
+      @path       = path
+      @mode       = mode || "w"
+      @options    = options
+      @repository = options.fetch(:repository) { GHFS.config.repository }
+      @branch     = options.fetch(:branch, nil)
     end
 
     def read
       @contents ||= begin
-                      encoded = GHFS.api.contents(GHFS.config.repository, path: path).content rescue nil
+                      encoded = api_entity.content rescue nil
 
                       if encoded.to_s.length > 0
                         Base64.decode64(encoded)
                       end
                     end
+    end
+
+    def puts(contents)
+    end
+
+    def write(contents)
+    end
+
+    def api_entity
+      @api_entity ||= GHFS.api.contents(repository, path: path)
+    rescue Octokit::NotFound
+      @new_file = true
+    rescue
+      nil
+    end
+
+    def new_file?
+      api_entity unless @api_entity
+      !@new_file.nil?
+    end
+
+    def exists?
+      !new_file?
+    end
+
+    def read_only?
+      mode == "r"
+    end
+
+    def append?
+      mode == "a" || mode == "a+"
+    end
+
+    def writable?
+      append? || mode == "w" || mode == "w+"
     end
   end
 end
--- spec/lib/github-fs/file_spec.rb
+++ spec/lib/github-fs/file_spec.rb
@@ -5,7 +5,18 @@ describe GHFS::File do
     GHFS::File.open("files/existing.txt")
   end
 
+  let(:new_file) { GHFS::File.open("files/new.txt") }
+
   it "lets me read the contents from a file" do
     expect(file.read).to match("This is some existing content")
+  end
+
+  it "lets me know if a file exists" do
+    expect(file).to be_exists
+  end
+
+  it "lets me know if the file is a new file" do
+    expect(new_file).to be_new_file
+    expect(new_file).not_to be_exists
   end
 end
